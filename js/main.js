694f0c2bca928c71be4d7aa3c8d4ddd5b9799f56
Bucky Schwarz <bucky@buckyschwarz.net>
2015-05-31 09:39:03 -0400

    added styling to canvas
    learned that setting a canvas' width or height attribute clears the
    context, including transformations and fill styles.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,33 @@ requirejs(['game_controller', 'renderer', 'performance_monitor'], function(GameC
 
     var $canvas = document.getElementById('canvas');
     var $performance = document.getElementById('performance');
+    var context = $canvas.getContext('2d');
 
-    var renderer = new Renderer($canvas.getContext('2d'));
+    var renderer = new Renderer(context);
     var performance_monitor = new PerformanceMonitor($performance);
 
     var controller = new GameController(renderer, performance_monitor);
     controller.init();
 
+    var scaleCanvas = function() {
+        var $container = document.getElementsByClassName('container')[0];
+        var border_width = parseInt(getComputedStyle($container)['border-width'], 10) * 2;
+
+        $canvas.width = $container.offsetWidth - border_width;
+        $canvas.height = $container.offsetHeight - border_width;
+        renderer.init();
+    };
+
+    scaleCanvas();
+
     $canvas.addEventListener('click', function(event) {
         controller.explosion({
             x: event.layerX,
             y: event.layerY
-        })
+        });
     }, false);
+
+     window.addEventListener('resize', function(event) {
+        scaleCanvas();
+     }, false);
 });
