c4a903acb74421dc1df0bc4fb5b26aa8c44c72f5
Bucky Schwarz <d.w.schwarz@gmail.com>
2015-07-23 18:06:58 -0400

    changed expanding_circle to explosion

--- js/engine/effects/explosion.js
+++ js/engine/effects/explosion.js
@@ -1,12 +1,12 @@
 define([], function() {
     'use strict';
 
-    var ExpandingCircle = function(circle, animation_duration) {
-        this.circle = circle;
+    var Explosion = function(explosion, animation_duration) {
+        this.explosion = explosion;
         this.animation_duration = animation_duration;
     };
 
-    ExpandingCircle.prototype = {
+    Explosion.prototype = {
         init: function() {
             this.total_animation_time = 0;
             this.time_left = this.animation_duration;
@@ -15,21 +15,21 @@ define([], function() {
         update: function(dt) {
             this.time_left = this.animation_duration - this.total_animation_time;
             if (this.time_left > 0) {
-                var distance_to_radius = this.circle.desired_radius - this.circle.current_radius;
+                var distance_to_radius = this.explosion.desired_radius - this.explosion.current_radius;
                 var radius_increase_step = distance_to_radius / this.time_left;
 
-                this.circle.current_radius += radius_increase_step * dt;
+                this.explosion.current_radius += radius_increase_step * dt;
             }
 
-            if (this.circle.current_radius >= this.circle.desired_radius) {
-                this.circle.current_radius = this.circle.desired_radius;
+            if (this.explosion.current_radius >= this.explosion.desired_radius) {
+                this.explosion.current_radius = this.explosion.desired_radius;
             }
 
             this.total_animation_time += dt;
         },
 
-        render: function(circle) {
-            this.circle(circle.x, circle.y, circle.current_radius, { strokeStyle: circle.color });
+        render: function(explosion) {
+            this.explosion(explosion.x, explosion.y, explosion.current_radius, { strokeStyle: explosion.color });
         }
     };
 
@@ -37,8 +37,8 @@ define([], function() {
         return Math.floor(Math.random() * 256);
     };
 
-    ExpandingCircle.CreateAtCoords = function(coords) {
-        var circle = new ExpandingCircle({
+    Explosion.CreateAtCoords = function(coords) {
+        var explosion = new Explosion({
             x: coords.x,
             y: coords.y,
             current_radius: 0,
@@ -46,10 +46,10 @@ define([], function() {
             color: 'rgb(' + randomByteInt() + ',' + randomByteInt() + ',' + randomByteInt() + ')'
         }, 200 + Math.ceil(Math.random() * 2000));
 
-        circle.init();
+        explosion.init();
 
-        return circle;
+        return explosion;
     };
 
-    return ExpandingCircle;
+    return Explosion;
 });
